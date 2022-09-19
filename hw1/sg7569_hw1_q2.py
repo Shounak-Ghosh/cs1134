d74e57f333cff1c0ebb7b9289c6f6d02a19d99ea
Shounak-Ghosh <shounak.ghosh.pi@gmail.com>
2022-09-19 14:41:06 -0400

    updated q2 code -- cant return new list

--- hw1/sg7569_hw1_q2.py
+++ hw1/sg7569_hw1_q2.py
@@ -1,10 +1,14 @@
 def shift(lst, k, dir='left'):
     # shifts original list by k positions to the left
     if dir == 'left':
-        return lst[k:] + lst[:k]
+        ans =  lst[k:] + lst[:k]
     elif dir == 'right':
-        return lst[-k:] + lst[:-k]
-
+        ans = lst[-k:] + lst[:-k]
+    
+    for i in range(len(lst)):
+            lst[i] = ans[i]
+    return lst
+        
 
 def main():
     lst = [1, 2, 3, 4, 5, 6]
