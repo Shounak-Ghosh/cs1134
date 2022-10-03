97b4c3a4d81b9fb67c8c6b8457608af86968ec21
Shounak-Ghosh <shounak.ghosh.pi@gmail.com>
2022-10-03 15:57:54 -0400

    corrected hw1 code

--- homework/hw1/sg7569_hw1_q3.py
+++ homework/hw1/sg7569_hw1_q3.py
@@ -1,20 +1,20 @@
 def sum_of_squares(n):
     ans = 0
-    for i in range(1, n+1):
+    for i in range(1, n):
         ans += i**2
     return ans
 
 def sum_of_squares2(n):
-    return sum([x**2 for x in range(1, n+1)])
+    return sum([x**2 for x in range(1, n)])
 
 def sum_of_odd_squares(n):
     ans = 0
-    for i in range(1, n+1, 2):
+    for i in range(1, n, 2):
         ans += i**2
     return ans
 
 def sum_of_odd_squares2(n):
-    return sum([x**2 for x in range(1, n+1, 2)])
+    return sum([x**2 for x in range(1, n, 2)])
 
 def main():
     print(sum_of_squares(10))
--- homework/hw1/sg7569_hw1_q5.py
+++ homework/hw1/sg7569_hw1_q5.py
@@ -1,14 +1,18 @@
 def fibs(n):
     f0 = 1
     f1 = 1
-    yield f0
-    yield f1
-    while n > 2:
-        f2 = f0 + f1
-        yield f2
-        f0 = f1
-        f1 = f2
-        n -= 1
+
+    if n == 1:
+        yield f0
+    else:
+        yield f0
+        yield f1
+        while n > 2:
+            f2 = f0 + f1
+            yield f2
+            f0 = f1
+            f1 = f2
+            n -= 1
 
 def main():
     for curr in fibs(8):
