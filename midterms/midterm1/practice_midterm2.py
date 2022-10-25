e674bb7be63ef5a959653f152da34104b1573cbc
Shounak-Ghosh <shounak.ghosh.pi@gmail.com>
2022-10-25 10:21:29 -0400

    midterm 1 files

--- midterms/midterm1/practice_midterm2.py
+++ midterms/midterm1/practice_midterm2.py
@@ -8,24 +8,27 @@ def revPrint(lst, low, high):
 def function2(lst):
     if len(lst) == 1:
         lst[0] = 0
-        return 2
-    return function2(lst[:len(lst)//2])
+        return 1
+    return  1 + function2(lst[:len(lst)//2])
 
 def harmonic(n):
     for i in range(1,n+1):
         yield 1/i
 
 def main():
-    lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    revPrint(lst, 0, len(lst)-1)
+    # lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    # revPrint(lst, 0, len(lst)-1)
 
-    b = [3,1,6,2]
-    print(function2(b))
-    print(b)
+    # b = [3,1,6,2]
+    # print(function2(b))
+    # print(b)
 
-    iters = 4
-    display_list = list(harmonic(iters))
-    print(display_list)
+    # iters = 4
+    # display_list = list(harmonic(iters))
+    # print(display_list)
+
+    l = [1] * 10000
+    print(function2(l))
 
 if __name__ == '__main__':
     main()
