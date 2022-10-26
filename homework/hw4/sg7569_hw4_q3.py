013ff9b35a197240c1b1b0bf981c9fe79ebc849e
Shounak-Ghosh <shounak.ghosh.pi@gmail.com>
2022-10-26 01:21:07 -0400

    initial q3 submission

--- homework/hw4/sg7569_hw4_q3.py
+++ homework/hw4/sg7569_hw4_q3.py
@@ -1,11 +1,16 @@
 def print_triangle(n):
+    if n <= 0:
+        return
     if n == 1:
         print("*")
     else:
         print_triangle(n-1)
         print("*"*n)
 
+
 def print_opposite_triangles(n):
+    if n <= 0:
+        return
     if n == 1:
         print("*")
         print("*")
@@ -14,13 +19,25 @@ def print_opposite_triangles(n):
         print_opposite_triangles(n-1)
         print("*"*n)
 
-# TODO: print ruler function
+
 def print_ruler(n):
-    pass
+    if n <= 0:
+        return
+    if n == 1:
+        print("-")
+    else:
+        print_ruler(n-1)
+        print("-"*n)
+        print_ruler(n-1)
+
 
 def main():
-    # print_triangle(4)
+    print_triangle(4)
+    print()
     print_opposite_triangles(4)
+    print()
+    print_ruler(4)
+
 
 if __name__ == '__main__':
     main()
