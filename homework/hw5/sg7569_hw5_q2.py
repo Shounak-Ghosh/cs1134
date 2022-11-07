55e9f52302fc54609875ac3a4ece385aa31c5c89
Shounak-Ghosh <shounak.ghosh.pi@gmail.com>
2022-11-07 18:03:09 -0500

    code formatting

--- homework/hw5/sg7569_hw5_q2.py
+++ homework/hw5/sg7569_hw5_q2.py
@@ -1,30 +1,29 @@
 from ArrayStack import ArrayStack
 
+
 class MaxStack:
     def __init__(self):
         self.stack = ArrayStack()
 
-    def is_empty(self):
-        return self.stack.is_empty()
-    
     def __len__(self):
         return len(self.stack)
-    
+
+    def is_empty(self):
+        return self.stack.is_empty()
+
     def push(self, item):
-        if self.is_empty(): 
-            self.stack.push((item,item)) # tuple of (item, max)
-        elif item > self.stack.top()[1]: # if item is greater than max
-            self.stack.push((item,item))
+        if self.is_empty():
+            self.stack.push((item, item))  # tuple of (item, max)
+        elif item > self.stack.top()[1]:  # if item is greater than max
+            self.stack.push((item, item))
         else:
             self.stack.push((item, self.stack.top()[1]))
-    
+
     def pop(self):
         return self.stack.pop()[0]
-    
+
     def top(self):
         return self.stack.top()[0]
-    
+
     def max(self):
         return self.stack.top()[1]
-
-    
