e7c5c6f6568d3d2e44c7e499e74bfe0b8c65a480
Shounak-Ghosh <shounak.ghosh.pi@gmail.com>
2022-11-07 19:12:50 -0500

    q3 code

--- homework/hw5/sg7569_hw5_q3.py
+++ homework/hw5/sg7569_hw5_q3.py
@@ -1,5 +1,45 @@
 from ArrayStack import ArrayStack
 from ArrayDeque import ArrayDeque
 
+# TODO do this question
+# general idea: stack adjacent to deque
+
+
 class MidStack:
-    pass
+    def __init__(self):
+        self.stack = ArrayStack()
+        self.deque = ArrayDeque()
+
+    def __len__(self):
+        return len(self.stack) + len(self.deque)
+
+    def is_empty(self):
+        return len(self) == 0
+
+    def push(self, val):
+        # base case
+        if self.stack.is_empty():
+            self.stack.push(val)
+        else:  # general case
+            if len(self.stack) == len(self.deque):
+                self.stack.push(self.deque.dequeue_first())
+            self.deque.enqueue_last(val)
+
+    def pop(self):
+        # print("stack: ", self.stack, "deque: ", self.deque)
+        popped = self.deque.dequeue_last()
+        if not self.stack.is_empty() and self.deque.is_empty():
+            self.deque.enqueue_last(self.stack.pop())
+        
+        return popped
+
+    def top(self):
+        if self.deque.is_empty():
+            return self.stack.top()
+        else:
+            return self.deque.last()
+
+    def mid_push(self, val):
+        self.stack.push(val)
+
+        
