2d10a3bf64af12625734ec2660e710e012a629af
Shounak-Ghosh <shounak.ghosh.pi@gmail.com>
2022-12-08 21:40:41 -0500

    need to compare right min to root as well

--- homework/hw8/sg7569_hw8_q4.py
+++ homework/hw8/sg7569_hw8_q4.py
@@ -3,16 +3,18 @@ from BinarySearchTreeMap import BinarySearchTreeMap
 def find_min_abs_difference(bst):
     # for each node, return the current min difference and the current max node  in the subtree
     def find_min_abs_difference_helper(node):
-        if not node: # base case -- return larg min_diff, and 0 for max val
-            return float('inf'), 0
+        if not node: # base case -- return large min_diff, 0 for max, and large number for min
+            return float('inf'), 0, float('inf')
         
-        l_min_diff, l_max = find_min_abs_difference_helper(node.left)
-        r_min_diff, r_max = find_min_abs_difference_helper(node.right)
+        l_min_diff, l_max, l_min = find_min_abs_difference_helper(node.left)
+        r_min_diff, r_max, r_min = find_min_abs_difference_helper(node.right)
 
-        min_diff = min(l_min_diff, r_min_diff, abs(node.item.key - l_max), abs(node.item.key - r_max))
+        # max of left subtree and min of right subtree are values closest to the root
+        min_diff = min(l_min_diff, r_min_diff, abs(node.item.key - l_max), abs(node.item.key - r_min))
         max_val = max(node.item.key, l_max, r_max)
+        min_val = min(node.item.key, l_min, r_min)
 
-        return min_diff, max_val
+        return min_diff, max_val, min_val
 
     return find_min_abs_difference_helper(bst.root)[0]
 
@@ -27,11 +29,25 @@ def main():
     bst[25] = None
     bst[1] = None
     bst[6] = None
-    
-    
-    bst.print_tree()
 
+    bst.print_tree()
     print(find_min_abs_difference(bst))
+    print()
+
+    # insert in level-order in order to get the right bst
+    bst = BinarySearchTreeMap()
+    bst[9] = None
+    # bst[7] = None
+    bst[20] = None
+    #bst[4] = None
+    bst[12] = None
+    bst[25] = None
+    #bst[1] = None
+    #bst[6] = None
+
+    bst.print_tree()
+    print(find_min_abs_difference(bst))
+
 
 if __name__ == '__main__':
     main()
